28a070ad1a376663b04c9c723da1e8f593017abc
dmsgus <dmsgus07@naver.com>
2018-08-17 17:17:22 +0900

    n

--- GitTest/src/TestMain.java
+++ GitTest/src/TestMain.java
@@ -1,8 +1,8 @@
 
 public class TestMain {
 
-	public static void main( String[] args) {
-		// TODO Auto-generated method stub 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
  
 	}
 
